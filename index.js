ddd041a6e4beff1ae5a320e9b1539edb37619019
Arushi <arushi1467.cse18@chitkara.edu.in>
2021-01-25 18:26:17 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,112 @@
+ const questions=[
+            {
+                question: "Which of the following function of Array object joins all elements of an array into a string?",
+                answers:[
+                    "concat()",
+                    "join()",
+                    "pop",
+                    "map()"
+                ],
+                correctAnswer:"join()",
+                score:1,
+            },
+            {
+                question: "Who invented JavaScript?",
+                answers: [
+                    "Douglas Crockford",
+                    "Sheryl Sandberg",
+                    "Brendan Eich",
+                    "abc"
+                ],
+                correctAnswer: "Brendan Eich",
+                score:1,
+            },
+            {
+                question:"Which of the following function of Array object applies a function simultaneously against two values of the array (from right-to-left) as to reduce it to a single value?",
+                answers:[
+                    "pop()",
+                    "push()",
+                    "reduce()",
+                    "reduceRight()"
+                ],
+                correctAnswer:"reduceRight()",
+                score:1,
+            },
+            {
+                question:"Which of the following function of String object returns the index within the calling String object of    e last occurrence of the specified value",
+                answers:[
+                    "lastIndexOf()",
+                    "search()",
+                    "substr()",
+                    "indexOf()"
+                ],
+                correctAnswer:"lastIndexOf()",
+                score:1,
+            },
+            {
+                question:"Which built-in method returns the character at the specified index?",
+                answers:[
+                    "characterAt()",
+                    "getCharAt()",
+                    "charAt()",
+                    "None of the above"
+                ],
+                correctAnswer:"charAt()",
+                score:1,
+            },
+            {
+                question: "Which tool can you use to ensure code quality?",
+                answers: [
+                    "Angular",
+                    "jQuery",
+                    "RequireS",
+                    "ESLint"
+                ],
+                correctAnswer: "ESLint",
+                score:1,
+            },
+            {
+                question:" Which of the following function of String object causes a string to be displayed in the specified color as if it were in a tag",
+                answers:[
+                    "fixed()",
+                    "fontcolor()",
+                    "blink()",
+                    "bold()"
+                ],
+                correctAnswer:"fontcolor()",
+                score:1,
+            },
+            {
+                question: "Which one of these is a JavaScript package manager?",
+                answers: [
+                    "Node.js",
+                    "TypeScript",
+                    "npm",
+                    "xyz"
+                ],
+                correctAnswer: "npm",
+                score:1,
+            },
+            {
+                question: "Who invented JavaScript?",
+                answers: [
+                    "Douglas Crockford",
+                    "Sheryl Sandberg",
+                    "Brendan Eich",
+                    "afghjk"
+                ],
+                correctAnswer: "Brendan Eich",
+                score:1,
+            },
+            {
+                question: "Which of the following function of Array object joins all elements of an array into a string?",
+                answers:[
+                        "concat()",
+                        "join()",
+                        "pop()",
+                        "map()"
+                ],
+                correctAnswer:"join()",
+                score:1,
+            },
+        ];
